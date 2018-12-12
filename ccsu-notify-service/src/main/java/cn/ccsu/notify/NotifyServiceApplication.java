400568d590add5035903013d0f7d926849457c1e
hangs.zhang <1078504296@qq.com>
2018-12-12 18:16:48 +0800

    站内消息推送 邮件消息推送

--- ccsu-notify-service/src/main/java/cn/ccsu/notify/NotifyServiceApplication.java
+++ ccsu-notify-service/src/main/java/cn/ccsu/notify/NotifyServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class NotifyServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(NotifyServiceApplication.class, args);
     }
 }
