d321c9285afdeb523948e91387079be3294be7d6
hangs.zhang <1078504296@qq.com>
2019-01-29 01:54:14 +0800

    完成首页大部分接口

--- ccsu-main-service/src/main/java/cn/ccsu/main/CcsuMainServiceApplication.java
+++ ccsu-main-service/src/main/java/cn/ccsu/main/CcsuMainServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CcsuMainServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CcsuMainServiceApplication.class, args);
     }
 
