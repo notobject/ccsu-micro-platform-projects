b33ad0d3c0bd66ffd80b8b2dacaa9cac4c7b1f5d
hangs.zhang <1078504296@qq.com>
2019-03-18 16:55:26 +0800

    环境变化，修改参数

--- ccsu-main-service/src/main/java/cn/ccsu/main/CcsuMainServiceApplication.java
+++ ccsu-main-service/src/main/java/cn/ccsu/main/CcsuMainServiceApplication.java
@@ -1,15 +1,30 @@
 package cn.ccsu.main;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 @MapperScan(basePackages = "cn.ccsu.main.dao")
 public class CcsuMainServiceApplication {
 
+    @Value("${tag: unkown}")
+    private String tag;
+
     public static void main(String[] args) {
         SpringApplication.run(CcsuMainServiceApplication.class, args);
+    }
+
+    @RequestMapping("/tag")
+    public String version() {
+        return "tag: " + tag;
     }
 
 }
