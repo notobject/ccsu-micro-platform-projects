532a7964cee25e2ee521217003de05730d2ceac5
hangs.zhang <1078504296@qq.com>
2019-03-19 09:56:39 +0800

    将ip配置修改为启动时动态注入

--- ccsu-user-service/src/main/java/cn/ccsu/user/UserServiceApplication.java
+++ ccsu-user-service/src/main/java/cn/ccsu/user/UserServiceApplication.java
@@ -1,20 +1,32 @@
 package cn.ccsu.user;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@RestController
 @MapperScan(basePackages = "cn.ccsu.user.dao")
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Value("${tag: unkown}")
+    private String tag;
+
+    @RequestMapping("/tag")
+    public String version() {
+        return "tag: " + tag;
     }
 
     @Bean
@@ -22,4 +34,5 @@ public class UserServiceApplication {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate;
     }
+
 }
